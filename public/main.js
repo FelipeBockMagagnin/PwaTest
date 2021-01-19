f17707666b56d97e25d0935831575e1deb9f6bfa
Felpi <felipebmagagnin@gmail.com>
2021-01-19 15:07:53 -0300

    add icon on map

--- public/main.js
+++ public/main.js
@@ -48,12 +48,30 @@ function showPosition(position) {
   var longitude = position.coords.longitude;
   var latitude = position.coords.latitude;
 
+  const iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude])),
+    name: 'Somewhere near Nottingham',
+  });
+
   var map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
-      })
+      }),
+      new ol.layer.Vector({
+        source: new ol.source.Vector({
+          features: [iconFeature]
+        }),
+        style: new ol.style.Style({
+          image: new ol.style.Icon({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            src: 'https://openlayers.org/en/latest/examples/data/icon.png'
+          })
+        })
+      })      
     ],
     view: new ol.View({
       center: ol.proj.fromLonLat([longitude, latitude]),
